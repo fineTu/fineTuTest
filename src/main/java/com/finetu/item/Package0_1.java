027c865767637a2ac2910028d782bcc1b289e59e
gaolifeng <gaolifeng@meituan.com>
2014-06-13 13:55:38 +0800

    init

---
+++ src/main/java/com/finetu/item/Package0_1.java
@@ -0,0 +1,15 @@
+package com.finetu.item;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gaolifeng
+ * Date: 14-6-13
+ * Time: 下午1:50
+ */
+public class Package0_1 {
+    public static void main(String args[]){
+
+    }
+
+    
+}
